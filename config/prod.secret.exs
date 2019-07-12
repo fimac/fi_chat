7299e0f8e629d61ca468862c1f8c6e6f9682e597
Fiona McCawley <fimccawley@gmail.com>
2019-07-12 14:20:56 +1000

    work

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,6 @@ config :fi_chat, FiChatWeb.Endpoint, secret_key_base: System.get_env("SECRET_KEY
 
 # Configure your database
 config :fi_chat, FiChat.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "fi_chat_prod",
-  pool_size: 10
+  ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
