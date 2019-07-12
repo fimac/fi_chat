260e8cd18b1e0eb90d4bf3dd3a39c6a66b6d862f
Fiona McCawley <fimccawley@gmail.com>
2019-07-12 13:32:43 +1000

    ssl false

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :fi_chat, FiChat.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  ssl: false
 
 # ## SSL Support
 #
